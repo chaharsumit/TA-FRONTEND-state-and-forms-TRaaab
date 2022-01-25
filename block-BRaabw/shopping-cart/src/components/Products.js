0618de8ba1b899cbb5d7b21b8ea476fd8cfc533f
Sumit Chahar <chahar.sumit888@gmail.com>
2022-01-25 17:46:40 +0530

    Add size and price filters for react shopping cart

---
+++ block-BRaabw/shopping-cart/src/components/Products.js
@@ -0,0 +1,21 @@
+function Products(props){
+  return (
+    <ul className="item-grid flex">
+      {
+        props.productList.map(item => (
+          <li key={item.id} className="grid-item">
+            <div className="image-container">
+              <img src={`products/${item.sku}_1.jpg`} />
+            </div>
+            <h4>{item.title}</h4>
+            <hr />
+            <p className="price">${item.price}</p>
+            <button className="btn">Add To Cart</button>
+          </li>
+        ))
+      }
+    </ul>
+  );
+}
+
+export default Products;
